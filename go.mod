483c9eda6b32b6c462f5df14c6f57416515c5816
tenglong.chen <359343287@qq.com>
2022-10-14 18:00:25 +0800

    feat: jaeger

--- go.mod
+++ go.mod
@@ -4,10 +4,13 @@ go 1.16
 
 require (
 	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
+	github.com/gin-gonic/gin v1.8.1
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/soheilhy/cmux v0.1.5
 	github.com/stretchr/testify v1.8.0 // indirect
 	github.com/uber/jaeger-client-go v2.26.0+incompatible
 	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
 	go.uber.org/atomic v1.8.0 // indirect
+	google.golang.org/grpc v1.50.0
 )
--- jaegerex/jaegerex.go
+++ jaegerex/jaegerex.go
@@ -3,22 +3,33 @@ package jaegerex
 import (
 	"context"
 	"io"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	openTracingLog "github.com/opentracing/opentracing-go/log"
 	"github.com/uber/jaeger-client-go"
 	"github.com/uber/jaeger-client-go/config"
 )
 
 type ILinkTracing interface {
-	Close() error
+	Close()
+	// WithHeaderSpan(http.Header) ITracingSpan
 	WithStartSpan(string) ITracingSpan
 	WithSpanFromContext(context.Context, string) ITracingSpan
 }
 
 type ITracingSpan interface {
 	Context() context.Context
-	SetTag(key string, value interface{})
 	Finish()
+	SetTag(key string, value interface{})
+	SetLogs(fields ...openTracingLog.Field)
+	Span() opentracing.Span
+}
+
+type ITracingSpanExt interface {
 }
 
 type linkTracingImpl struct {
@@ -26,15 +37,42 @@ type linkTracingImpl struct {
 	reportAddr  string // host:port
 
 	tracer opentracing.Tracer
-	closer io.Closer
+	closer io.Closer // todo: 链接使用完未关闭，最后一个次的链接会有丢失。 优化：在程序退出时调用close
 }
 
-func (t linkTracingImpl) Close() error {
+func (t linkTracingImpl) Close() {
 	if t.closer != nil {
-		return t.closer.Close()
+		t.closer.Close()
+	}
+}
+
+func (t linkTracingImpl) WithHeaderSpan(header http.Header, name string) (ts ITracingSpan, err error) {
+	var span opentracing.Span
+	spanCtx, err := t.tracer.Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(header))
+	if err != nil {
+		span = t.tracer.StartSpan(name)
+	} else {
+		span = t.tracer.StartSpan(
+			name,
+			opentracing.ChildOf(spanCtx),
+			ext.SpanKindRPCClient,
+		)
 	}
 
-	return nil
+	ts = &tracingSpanImpl{
+		span: span,
+	}
+
+	return
+}
+
+func (t linkTracingImpl) Inject(header http.Header, tracingSpan ITracingSpan) {
+	err := t.tracer.Inject(tracingSpan.Span().Context(), opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(header))
+	if err != nil {
+		tracingSpan.SetLogs(openTracingLog.String("inject_error", err.Error()))
+	}
+
+	// todo: 预留添加traceID 放至ctx
 }
 
 func (t linkTracingImpl) WithStartSpan(name string) ITracingSpan {
@@ -59,12 +97,24 @@ type tracingSpanImpl struct {
 }
 
 func (s tracingSpanImpl) Context() context.Context {
+	if s.ctx == nil {
+		s.ctx = context.Background()
+	}
+
 	return s.ctx
 }
 
+func (s tracingSpanImpl) Span() opentracing.Span {
+	return s.span
+}
+
 func (s tracingSpanImpl) SetTag(key string, value interface{}) {
-	if s.span != nil {
-		s.span.SetTag(key, value)
+	s.span.SetTag(key, value)
+}
+
+func (s tracingSpanImpl) SetLogs(fields ...openTracingLog.Field) {
+	if len(fields) > 0 {
+		s.span.LogFields(fields...)
 	}
 }
 
@@ -74,21 +124,33 @@ func (s tracingSpanImpl) Finish() {
 	}
 }
 
+// 暂时方案不存在待久化
 func New(serviceName, reportAddr string) ILinkTracing {
 	cfg := config.Configuration{
 		ServiceName: serviceName,
 		Sampler: &config.SamplerConfig{
-			Type:  "const", // 常量采样
-			Param: 1,       // 开启所有轨迹采样
+			Type:  jaeger.SamplerTypeConst, // 常量采样
+			Param: 1,                       // 开启所有轨迹采样
 		},
 		Reporter: &config.ReporterConfig{
-			LogSpans:           true,
 			LocalAgentHostPort: reportAddr,
+			LogSpans:           true,
 		},
+		// Collector 配置
+		// Reporter: &config.ReporterConfig{
+		// 	BufferFlushInterval: 100 * time.Millisecond,
+		// 	CollectorEndpoint:   reportAddr,
+		// 	LogSpans:            false,
+		// },
 	}
-	tracer, closer, err := cfg.NewTracer(config.Logger(jaeger.StdLogger))
+	tracer, closer, err := cfg.NewTracer(
+		config.Logger(jaeger.StdLogger),
+		// config.ZipkinSharedRPCSpan(true), 需要单独开启
+	)
 	if err != nil {
-		panic("new jaeger configuration failed: " + err.Error())
+		log.Println("new jaeger configuration failed: ", err)
+		os.Exit(1)
+		return nil
 	}
 	opentracing.SetGlobalTracer(tracer)
 
