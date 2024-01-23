5429756e9e59e7d9f8c89f5289cc93eb90eb3eee
xm-chentl <359343287@qq.com>
2024-01-23 16:50:46 +0800

    refactor: route handler feat: add new web socket connect

--- frame/defaut.go
+++ frame/defaut.go
@@ -4,33 +4,37 @@ import (
 	"fmt"
 	"net/http"
 	"reflect"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	"github.com/xm-chentl/gocore/frame/handles"
 )
 
-type RouteHandle []string
+type NumberSegments int
 
-func (r RouteHandle) Relative() (res string) {
-	for _, v := range r {
-		res += "/:" + v
+func (r NumberSegments) Relative() (res string) {
+	for i := 0; i < int(r); i++ {
+		res += fmt.Sprintf("/:%d", i)
 	}
+
 	return
 }
 
-func (r RouteHandle) Route(ctx *gin.Context) (res string) {
+func (r NumberSegments) Route(ctx *gin.Context) (res string) {
 	vArr := make([]string, 0)
-	for _, v := range r {
-		vArr = append(vArr, ctx.Param(v))
+	for i := 0; i < int(r); i++ {
+		vArr = append(vArr, ctx.Param(strconv.Itoa(i)))
 	}
 	res = "/" + strings.Join(vArr, "/")
 
 	return
 }
 
-func NewHandle(method string, routeHandle RouteHandle) GinOption {
-	relativePath := routeHandle.Relative()
+func NewHandle(method string, num NumberSegments) GinOption {
+	relativePath := num.Relative()
 	return func(g *gin.Engine) {
 		g.Handle(method, relativePath, func(ctx *gin.Context) {
 			var err error
@@ -47,7 +51,7 @@ func NewHandle(method string, routeHandle RouteHandle) GinOption {
 				ctx.JSON(http.StatusOK, resp)
 			}()
 
-			route := routeHandle.Route(ctx)
+			route := num.Route(ctx)
 			fmt.Println("route >>> ", route)
 			if !handles.Has(route) {
 				err = ErrHandleNotExist
@@ -72,6 +76,30 @@ func NewHandle(method string, routeHandle RouteHandle) GinOption {
 			}
 
 			resp.Data, err = handler.Call(ctx)
+		})
+	}
+}
+
+type UpgradeFunction func(ctx *gin.Context, conn *websocket.Conn) error
+
+var wsUpgrader = websocket.Upgrader{
+	ReadBufferSize:   1024,
+	WriteBufferSize:  1024,
+	HandshakeTimeout: 5 * time.Second,
+	// 取消ws跨域校验
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func NewWebSocket(relativePath string, handle UpgradeFunction) GinOption {
+	return func(g *gin.Engine) {
+		g.GET(relativePath, func(ctx *gin.Context) {
+			conn, err := wsUpgrader.Upgrade(ctx.Writer, ctx.Request, nil)
+			if err != nil {
+				return
+			}
+			handle(ctx, conn)
 		})
 	}
 }
